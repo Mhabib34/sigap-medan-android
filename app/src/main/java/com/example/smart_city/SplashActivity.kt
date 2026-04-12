78b52c334b73190a200bb8089250b1cde2a4fbe0
Muhammad Habib <mhabib34official@gmail.com>
2026-04-12 11:18:19 +0700

    feat: add register activity

--- app/src/main/java/com/example/smart_city/SplashActivity.kt
+++ app/src/main/java/com/example/smart_city/SplashActivity.kt
@@ -15,7 +15,7 @@ class SplashActivity : AppCompatActivity() {
 
         // Pindah ke MainActivity setelah 2.5 detik
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, RegisterActivity::class.java))
             finish()
         }, 2500)
     }
